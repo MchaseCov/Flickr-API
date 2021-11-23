697ee7ec58ab3b6d585d894c2ef3871f655252ae
Chase Covington <mchasecov@gmail.com>
2021-11-23 16:22:42 -0500

    refactor: move variable definitions into private methods

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,18 +1,28 @@
 class StaticPagesController < ApplicationController
   require 'flickr'
   def index
-    flickr = Flickr.new
+    @flickr = Flickr.new
+    fetch_uid
+    fetch_photos
+  end
+
+  private
+
+  def fetch_uid
     @user_id =
       if params[:username].present?
-        flickr.people.findByUsername(username: params[:username]).id
+        @flickr.people.findByUsername(username: params[:username]).id
       else
         params[:user_id]
       end
+  end
+
+  def fetch_photos
     @photos =
       if @user_id.present?
-        flickr.photos.search(user_id: @user_id)
+        @flickr.photos.search(user_id: @user_id)
       else
-        flickr.photos.getRecent.first(15)
+        @flickr.photos.getRecent.first(15)
       end
   end
 end
