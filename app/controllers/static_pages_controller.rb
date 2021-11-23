cd9eca0e674a32029df4d90a2d8149ee51cffd78
Chase Covington <mchasecov@gmail.com>
2021-11-23 15:55:30 -0500

    feat: index pull images from flickr

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,12 @@
 class StaticPagesController < ApplicationController
+  require 'flickr'
+  def index
+    flickr = Flickr.new
+    @photos =
+      if params[:user_id].present?
+        flickr.photos.search(user_id: params[:user_id])
+      else
+        flickr.photos.getRecent.first(15)
+      end
+  end
 end
